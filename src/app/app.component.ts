5c430035565b706b392e536c125a3c5425d3421a
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2021-02-22 09:56:26 -0700

    removed unneccesary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-// import { MovieCardComponent } from './movie-card/movie-card.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
